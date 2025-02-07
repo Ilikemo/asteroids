4d82824a040d0ebaa6f3857e0da1b306b9dd8316
Ilikemo <codyrockwall101@yahoo.com>
2025-02-06 20:43:25 -0700

    trying to finish up the circleshape and player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -14,6 +15,7 @@ def main():
                 return
         
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
--- player.py
+++ player.py
@@ -1,3 +1,8 @@
+import pygame
+from constants import *
+from circleshape import CircleShape
+
+
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(PLAYER_RAIDUS)
