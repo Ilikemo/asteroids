4bec9f055902f92f04cc56ed86a7158eb861b463
Ilikemo <codyrockwall101@yahoo.com>
2025-02-06 22:37:57 -0700

    added asteroids that are spawning and moving

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
 import pygame
 from constants import *
 from player import Player
-
+from asteroid import Asteroid
+from asteroidfield import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0
+    
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
 
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
+    dt = 0
+    
+    print("Number of objects in updateable:", len(updateable))
+    print("Number of objects in drawable:", len(drawable))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        updateable.update(dt)
+        
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
+        
+
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
