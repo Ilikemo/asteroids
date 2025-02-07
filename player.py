8bdee0a642a586750e832b4016a663931c7a4181
Ilikemo <codyrockwall101@yahoo.com>
2025-02-06 20:47:15 -0700

    fixing typo in player.py

--- player.py
+++ player.py
@@ -5,7 +5,7 @@ from circleshape import CircleShape
 
 class Player(CircleShape):
     def __init__(self, x, y):
-        super().__init__(x, y, PLAYER_RAIDUS)
+        super().__init__(x, y, PLAYER_RADIUS)
         rotation = 0
     
     # in the player class
